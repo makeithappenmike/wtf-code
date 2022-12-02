b9acf663862f8767e59551d670a99c2c86b617eb
MakeithappenMike <omichaelaaron@gmail.com>
2022-12-01 21:00:36 -0500

    got dotenv working

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,9 +1,7 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
 
-const MONGODB_URI = 'test'
-
-mongoose.connect(MONGODB_URI, {
+mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
