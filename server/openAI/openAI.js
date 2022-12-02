9390a0090a0e8e1f48206a3afe631d8fbc2cdf8d
jon-shogren <jon.shogren@mailchimp.com>
2022-12-02 15:42:38 -0500

    openAI proof of concept function

---
+++ server/openAI/openAI.js
@@ -0,0 +1,36 @@
+const { Configuration, OpenAIApi } = require("openai");
+const configuration = new Configuration({
+  // need to get env working
+//   apiKey: process.env.OPENAI_API_KEY,
+  
+});
+
+// take in a snippet and input it in the prompt with the explainer string
+const snippet = "//javascript\nfunction myFunction(p1, p2) {\n  return p1 * p2;\n}\n";
+
+// if we don't add the explainer, it doesn't explain!
+const classExplainer = "\"\"\"\nHere's what the above class is doing:\n1.";
+const functionExplainer = "\"\"\"\nHere's what the above function is doing:\n1.";
+const codeExplainer = "\"\"\"\nHere's what the above code is doing:\n1.";
+
+
+async function code() {
+    const openai = new OpenAIApi(configuration);
+    const response = await openai.createCompletion({
+      model: "code-davinci-002",
+      prompt: snippet + functionExplainer,
+      temperature: 0,
+      max_tokens: 150,
+      top_p: 1,
+      frequency_penalty: 0,
+      presence_penalty: 0,
+      stop: ["\"\"\""],
+    });
+    
+    // going to need this data returned, right?
+    const answer = response['data']['choices'][0].text;
+    console.log(answer);
+    return answer;
+}
+
+code();
