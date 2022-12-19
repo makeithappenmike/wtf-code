d5e7d41fc009e94b4039a069612350e701e28f1b
MakeithappenMike <omichaelaaron@gmail.com>
2022-12-19 14:11:50 -0600

    removing Google reference

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -16,7 +16,7 @@ const About = () => {
                 <h2 style={{ textAlign: 'center' }}>Welcome to WTFcode!</h2>
                 <p>Let's face it, as developers, at somepoint we find ourselves asking <em>'what the f*@k is this code actually doing?'</em>. Its a perfectly valid question, and sometimes easier to answer than others.</p>
                 <p><b>WTFcode</b> sets out to help developers answer that burning question by leveraging state-of-the-art technology with a bit of customization along the way.</p>
-                <p>By tapping into Google's <a href='https://openai.com/' target='blank' alt='OpenAI'>OpenAI</a> technology, we're able to provide secure, personalized access to one of their API Playgrounds that uses Artifical Intelligence to read and explain your code for you.</p>
+                <p>By tapping into <a href='https://openai.com/' target='blank' alt='OpenAI'>OpenAI's</a> technology, we're able to provide secure, personalized access to one of their API Playgrounds that uses Artifical Intelligence to read and explain your code for you.</p>
                 <p>By scanning billions of lines of code, OpenAi can provide some amazing context -- and its growing and learning as it goes.</p>
                 <p>Use our Code editor to analyze your snippets and save them to a database for future reference or share with a friend.</p>
                 <p>Happy coding!<br />Your friends at WTFcode</p>
